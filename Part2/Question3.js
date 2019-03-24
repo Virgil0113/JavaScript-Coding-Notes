3592d513b58d706ff511b14172e3b478088f8cee
Virgil0113 <361275898@qq.com>
2019-03-24 23:13:47 +0800

    Create Question3.js

---
+++ Part2/Question3.js
@@ -0,0 +1,16 @@
+//题目： 输入一个链表，按链表值从头到尾的顺序返回一个ArrayList.
+//思路：从头遍历链表值以unshift的方式链表值存入一个新数组，从头打印数组即可/
+
+/*function ListNode(x){
+    this.val = x;
+    this.next = null;
+}*/
+
+function printListFromTailToHead(head) {
+	var res = [];
+	while (head != null) {
+		res.unshift(head.val)
+		head = head.next;
+	}
+    return res;
+}
