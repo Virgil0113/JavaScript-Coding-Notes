c0c1862be0caa85df58910571faa1b8c54cf1b0e
Virgil0113 <361275898@qq.com>
2019-03-26 23:27:46 +0800

    Update Chapter1.js

--- Part1/Chapter1.js
+++ Part1/Chapter1.js
@@ -1,13 +1,16 @@
 // 递归方式
 function fibonacci(num) {
-  if (num === 1 || num === 2) { //1和2的斐波那契是1
+  if (num == 0) return 0;
+  if (num == 1 || num == 2) { //1和2的斐波那契是1
       return 1;
   }
   return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 // 非递归方式
-function fibonacci(num) {
+function fibonacci(num) { 
+  if (num == 0) return 0;
+  if (num == 1 || num == 2) return 1;
   var n1 = 1,
       n2 = 1,
       n = 0;
